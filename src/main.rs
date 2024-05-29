9f4d1d4177c354bac84706274af3602f1868636e
Michael Lueckl <mlueckl@pm.me>
2024-05-29 12:58:43 +0200

    Add type to handle data

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,49 @@ use std::fs;
 use std::io;
 use std::path::{Path, PathBuf};
 
+struct Dirs {
+    base_path: PathBuf,
+    directories: Vec<PathBuf>,
+    files: Vec<PathBuf>,
+}
+
+impl Dirs {
+    fn new(base_path: PathBuf) -> Self {
+        Self {
+            base_path,
+            directories: Vec::new(),
+            files: Vec::new(),
+        }
+    }
+
+    fn parse(&mut self, entries: Vec<PathBuf>) {
+        for e in entries {
+            match e.is_dir() {
+                true => self.directories.push(e),
+                false => self.files.push(e),
+            }
+        }
+    }
+}
+
 fn dir_collect_entries(input_dir: &Path) -> io::Result<Vec<PathBuf>> {
-    let mut entries = fs::read_dir(input_dir)?
+    let entries = fs::read_dir(input_dir)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
-
-    entries.sort();
 
     Ok(entries)
 }
 
-fn dir_get_current_entries() -> Vec<PathBuf> {
-    let cdir = current_dir().unwrap();
-    let e = dir_collect_entries(cdir.as_path());
+fn dir_get_current_entries() -> Dirs {
+    let mut current_dir = Dirs::new(current_dir().unwrap());
+    let collection = dir_collect_entries(current_dir.base_path.as_path());
+    current_dir.parse(collection.unwrap());
 
-    e.unwrap()
+    current_dir
 }
 
 fn main() {
-    let current = dir_get_current_entries();
-    println!("{:?}", current);
+    let current_dir = dir_get_current_entries();
+    println!("Directories: {:?}", current_dir.directories);
+    println!("Files: {:?}", current_dir.files);
 }
