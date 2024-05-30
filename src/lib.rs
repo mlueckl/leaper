8efba88f3db028baaafa875e63e02ca5916848a5
Michael Lueckl <mlueckl@pm.me>
2024-05-30 17:33:21 +0200

    Logic to follow all sub folders

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fs::read_dir;
+use std::io;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 pub fn bash(cmd: String) {
@@ -9,4 +12,83 @@ pub fn bash(cmd: String) {
         .expect("Failed to spawn command")
         .wait_with_output()
         .expect("Failed to wait for command");
+}
+
+fn dir_collect_entries(input_dir: &Path) -> io::Result<Vec<PathBuf>> {
+    let entries = read_dir(input_dir)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    Ok(entries)
+}
+
+pub fn dir_get_current_entries(mut current_dir: Dirs) -> Dirs {
+    if let Ok(collection) = dir_collect_entries(current_dir.base_path.as_path()) {
+        current_dir.entries.extend(collection);
+    };
+
+    current_dir
+}
+#[derive(Debug)]
+pub struct Dirs {
+    base_path: PathBuf,
+    entries: Vec<PathBuf>,
+    needle: String,
+    dirs_followed: bool,
+}
+
+impl Dirs {
+    pub fn new(base_path: PathBuf, needle: String) -> Self {
+        Self {
+            base_path,
+            entries: Vec::new(),
+            needle,
+            dirs_followed: false,
+        }
+    }
+
+    fn _follow_dirs(&mut self) {
+        let mut new_entries = Vec::new();
+        let mut had_dirs = false;
+
+        for e in self.entries.drain(..) {
+            if e.is_dir() {
+                if let Ok(dir_entries) = dir_collect_entries(e.as_path()) {
+                    new_entries.extend(dir_entries);
+                }
+                had_dirs = true;
+            } else {
+                new_entries.push(e);
+            }
+        }
+
+        if had_dirs == false {
+            self.dirs_followed = true;
+        }
+
+        self.entries = new_entries;
+    }
+
+    fn _exists(&self) -> PathBuf {
+        for entry in &self.entries {
+            if entry.ends_with(&self.needle) {
+                return entry.to_owned();
+            }
+        }
+
+        PathBuf::new()
+    }
+
+    pub fn find(mut self) -> PathBuf {
+        while self.dirs_followed == false {
+            let exists = self._exists();
+            if exists.as_os_str().is_empty() == false {
+                return exists;
+            }
+
+            self._follow_dirs();
+        }
+
+        PathBuf::new()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,5 @@
 use std::env::{args, current_dir};
-use std::fs::read_dir;
-use std::io;
-use std::path::{Path, PathBuf};
 use std::process;
-
-struct Dirs {
-    base_path: PathBuf,
-    entries: Vec<PathBuf>,
-}
-
-impl Dirs {
-    fn new(base_path: PathBuf) -> Self {
-        Self {
-            base_path,
-            entries: Vec::new(),
-        }
-    }
-}
-
-fn dir_collect_entries(input_dir: &Path) -> io::Result<Vec<PathBuf>> {
-    let entries = read_dir(input_dir)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-
-    Ok(entries)
-}
-
-fn dir_get_current_entries() -> Dirs {
-    let mut current_dir = Dirs::new(current_dir().unwrap());
-    let collection = dir_collect_entries(current_dir.base_path.as_path());
-    current_dir.entries.extend(collection.unwrap());
-
-    current_dir
-}
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -42,15 +9,16 @@ fn main() {
         process::exit(0);
     }
 
-    let needle = &args[2];
-    let directory = dir_get_current_entries();
+    let needle = String::from(&args[1]);
+    let mut directory = leap::Dirs::new(current_dir().unwrap(), needle);
+    directory = leap::dir_get_current_entries(directory);
 
-    println!("Searching for {}", needle);
+    let found = directory.find();
 
-    for entry in &directory.entries {
-        if entry.ends_with(needle) {
-            println!("Leaping to {}", entry.display());
-            leap::bash(format!("cd {} || exit 1\n$SHELL", entry.display()))
-        }
+    if found.as_os_str().is_empty() == false {
+        leap::bash(format!("cd {} || exit 1\n$SHELL", found.display()));
+        println!("Leaping to {}", found.display());
+    } else {
+        println!("{} not found", &args[1]);
     }
 }
