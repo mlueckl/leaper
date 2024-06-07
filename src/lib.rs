d331e882e1eeb66ca135988613ffadf07530f5f2
Michael Lueckl <mlueckl@pm.me>
2024-06-07 19:04:53 +0200

    Basic logging of found path

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use clap::{command, Arg, ArgMatches};
-use std::fs::{read_dir, ReadDir};
-use std::io;
+use std::env::current_dir;
+use std::fs::{read_dir, File, OpenOptions, ReadDir};
+use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 
 /// Handle and return CL arguments
@@ -27,9 +28,28 @@ pub fn args_handler() -> ArgMatches {
     match_results
 }
 
+/// Initialize File with options
+pub fn init_file(write: bool, create: bool, append: bool, path: &Path) -> io::Result<File> {
+    OpenOptions::new()
+        .write(write)
+        .create(create)
+        .append(append)
+        .open(&path)
+}
+
+pub fn write_to_file(content: &str) -> io::Result<()> {
+    let path = Path::new(current_dir().unwrap().as_path()).join("found.txt");
+
+    if let Ok(mut file) = init_file(true, true, true, path.as_path()) {
+        file.write(format!("{}\r", content).as_bytes())?;
+    }
+
+    Ok(())
+}
+
 /// Reads entries in provided directory
-pub fn get_read_dir(input_dir: &Path, is_upward: bool) -> io::Result<ReadDir> {
-    let dir = if is_upward {
+pub fn get_read_dir(input_dir: &Path, go_upward: bool) -> io::Result<ReadDir> {
+    let dir = if go_upward {
         input_dir
             .parent()
             .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Dir has no parent"))?
@@ -51,8 +71,8 @@ pub fn extract_entries(dir_entries: ReadDir) -> io::Result<Vec<PathBuf>> {
 }
 
 /// Return all entries for given location
-pub fn get_entries(path: &Path, is_upward: bool) -> io::Result<Vec<PathBuf>> {
-    match get_read_dir(path, is_upward) {
+pub fn get_entries(path: &Path, go_upward: bool) -> io::Result<Vec<PathBuf>> {
+    match get_read_dir(path, go_upward) {
         Ok(dir_entries) => extract_entries(dir_entries),
         Err(err) => return Result::Err(err),
     }
@@ -69,11 +89,11 @@ fn find_target(entries: &[PathBuf], target: &str) -> Option<PathBuf> {
 }
 
 /// Find target and return if found
-pub fn find(mut entries: Vec<PathBuf>, target: &str, is_upward: bool) -> Option<PathBuf> {
+pub fn find(mut entries: Vec<PathBuf>, target: &str, go_upward: bool) -> Option<PathBuf> {
     let mut found = find_target(&entries, target);
 
     while found.is_none() {
-        if let Some(unsearched_entries) = follow(&entries, is_upward) {
+        if let Some(unsearched_entries) = follow(&entries, go_upward) {
             found = find_target(&unsearched_entries, target);
             entries = unsearched_entries;
         } else {
@@ -90,20 +110,20 @@ pub fn find(mut entries: Vec<PathBuf>, target: &str, is_upward: bool) -> Option<
 }
 
 /// Follow directories and return unsearched entries
-pub fn follow(entries: &[PathBuf], is_upward: bool) -> Option<Vec<PathBuf>> {
+pub fn follow(entries: &[PathBuf], go_upward: bool) -> Option<Vec<PathBuf>> {
     let mut unsearched_entries: Vec<PathBuf> = Vec::new();
 
     // Ignore directories and files from entries, already established that target was not found
     for e in entries {
         // Follow directories and add their elements to unsearched_entries
         if e.is_dir() {
-            if is_upward {
+            if go_upward {
                 if e.parent().is_some() {
                     return Some(vec![e.parent().unwrap().to_path_buf()]);
                 }
             }
 
-            if let Ok(sub_entries) = get_entries(e.as_path(), is_upward) {
+            if let Ok(sub_entries) = get_entries(e.as_path(), go_upward) {
                 for se in sub_entries {
                     unsearched_entries.push(se);
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use leaper::{find, get_entries};
+use leaper::{find, get_entries, write_to_file};
 use std::env::current_dir;
 
 fn main() {
     // Handle CL arguments
     let args = leaper::args_handler();
     let target = args.get_one::<String>("target").unwrap();
-    let is_upward = args.get_flag("up");
+    let go_upward = args.get_flag("up");
 
     // Get directories & files of current location
-    if let Ok(entries) = get_entries(current_dir().unwrap().as_path(), is_upward) {
-        if let Some(found) = find(entries, target, is_upward) {
+    if let Ok(entries) = get_entries(current_dir().unwrap().as_path(), go_upward) {
+        if let Some(found) = find(entries, target, go_upward) {
+            write_to_file(&found.display().to_string()).unwrap();
+
             if found.is_file() {
                 println!("{}", found.parent().unwrap().display());
             } else {
